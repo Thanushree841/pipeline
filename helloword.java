32b1e77b7c90b7a447e94ad726ce7a668f742224
Thanushree841 <thanushreethanu53@gmail.com>
2025-07-16 15:33:12 +0530

    Update helloword.java

--- helloword.java
+++ helloword.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, SonarQube!");
+        System.out.println("Hello, SonarQube !");
     }
 }
